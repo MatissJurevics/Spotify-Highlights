d5507b56fe7ee1208433d171a8583058515de95c
matiss <matissjurevics@gmail.com>
2022-05-15 16:07:39 +0100

    creating my own nav

---
+++ src/components/sidebar/_normalNav.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+   name: "NormalNav" 
+}
+</script>
+
+<template>
+    <nav class="transition -translate-x-full md:translate-x-0 bg-slate-700 h-full w-1/5">
+
+    </nav>
+</template>
---
+++ src/components/sidebar/_smallNav.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: "SmallNav"
+
+}
+</script>
+
+<template>
+    <nav>
+
+    </nav>
+</template>
